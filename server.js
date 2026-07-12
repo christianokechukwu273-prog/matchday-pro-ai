a77687f1a935d6cf4c2ab9cd53d9ff01b0fff1e1
christianokechukwu273-prog <christianokechukwu273@gmail.com>
2026-07-12 17:53:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.json({
-    app: "MATCHDAY PRO AI",
-    status: "Running",
-    message: "Football Prediction Backend is Live!"
-  });
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 const PORT = process.env.PORT || 3000;
